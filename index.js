d9443d80667dca16ed75edc6b3398007aedec95d
Alexander Pichler <alexander.pichler91@icloud.com>
2018-04-18 17:25:47 +0200

    width/height in global variables

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let boi;
-
+let width = 1200;
+let height = 800;
 
 function setup() {
-    createCanvas(1200, 800);
+    createCanvas(width, height);
     rectMode(CENTER)
     translate(900, 800)
     boi = new Player();
