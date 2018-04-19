f314978ebc9965dcbebd6fcae0c2768f7cbaef95
Alexander Pichler <alexander.pichler91@icloud.com>
2018-04-19 14:27:59 +0200

    push pop fix

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ let height = 800;
 function setup() {
     createCanvas(width, height);
     rectMode(CENTER)
+    push()
     translate(width / 2, height / 2)
     boi = new Player()
     rooms[0][0] = new Room
     Room.generateAdjacent(0, 0)
-    console.log(rooms[0][0].doors["x"], rooms[0][0].doors["y"])
+    pop()
 }
 
 
 function draw() {
-    background("lightgrey");
-    rooms[0][0].draw();
-    boi.draw();
+    push()
+    translate(width / 2, height / 2)
+    background("lightgrey")
+    rooms[0][0].draw()
+    boi.draw()
+    pop()
 }
