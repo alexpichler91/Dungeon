2cd3c400ba9a0b3afb48498ed57c863ea3fd6b72
Alexander Pichler <alexander.pichler91@icloud.com>
2018-04-19 18:30:08 +0200

    rm merge markers

--- player.js
+++ player.js
@@ -1,8 +1,13 @@
 function Player() {
+    this.pos=createVector(500,500)
+    this.acc=createVector(0,0)
+    this.maxspeed=10;
+    this.big = 20;
+
     this.pos = createVector(width / 2, height / 2)    // Position des Spielers
     this.big = 20;                      //Größe des Spielers
     this.acc = createVector(0,0)        //Beschleunigung ...
-    this.accspeed=0.5;                     //Wie schnell er Beschleunigt        
+    this.accspeed=0.5;                  //Wie schnell er Beschleunigt
 
     this.draw = function(){
         if(keyIsDown(87)){
@@ -20,7 +25,32 @@ function Player() {
 
         this.acc.mult(0.95)
         this.pos.add(this.acc)
-        
+        if(keyIsDown(87)){
+            this.acc.y-=0.5
+        }
+        if(keyIsDown(83)){
+            this.acc.y+=0.5
+        }
+        if(keyIsDown(68)){
+            this.acc.x+=0.5
+        }
+        if(keyIsDown(65)){
+            this.acc.x-=0.5
+        }
+
+        this.acc.mult(0.95)
+        this.pos.add(this.acc)
+
+    push()
+    translate(this.pos.x,this.pos.y)
+    fill("red")
+    line(0,0,0,-20)
+    ellipse(0,0,this.big,this.big)
+
+    pop()
+    }
+}
+
         push()
         fill("red");
         translate(this.pos.x, this.pos.y)
@@ -28,11 +58,11 @@ function Player() {
         line(0,0,0,-20)
         ellipse(0,0,this.big,this.big)
         pop()
-        
+
     }
 }
 
 function FieldOfView() {
-    this.dpos= createVector((mouseX-boi.pos.x)*-1 , (mouseY-boi.pos.y)*-1) 
+    this.dpos= createVector((mouseX-boi.pos.x)*-1 , (mouseY-boi.pos.y)*-1)
     return atan2(this.dpos.x,this.dpos.y)*-1
 }
