d61cea2ba4f66a076984cfd0c51308c88ae47929
Speedy21 <David.Fankhauser@htl-braunau.at>
2018-04-25 11:12:36 +0200

    v1 Items

--- index.js
+++ index.js
@@ -14,17 +14,13 @@ function setup() {
     boi = new Player()
     rooms[roomPos.x][roomPos.y] = new Room()
     overlay=new Overlay()
-    spawnItem = new SpawnItems();
 
     Room.generateAdjacent(0, 0)
     let obsSize = createVector(floor(random(minObstacleX, maxObstacleX)), floor(random(minObstacleY, maxObstacleY)))
     let obsPos = createVector(floor(random(-(rooms[0][0].size.x - obsSize.x) / 2, (rooms[0][0].size.x - obsSize.x) / 2)), floor(random(-(rooms[0][0].size.y - obsSize.y) / 2, (rooms[0][0].size.y - obsSize.y) / 2)))
     let obs = new Obstacle_Stone(obsPos, obsSize)
     rooms[0][0].addObstacle(obs)
-
-    /* einfügen wenn in neuer raum*/
-    spawnItem.spawnItems();
-    /* einfügen wenn in neuer raum*/
+    
 }
 
 
@@ -32,7 +28,6 @@ function draw() {
     background("lightgrey");
     rooms[0][0].draw();
     rooms[0][0].obstacles[0].draw();
-    itemsInRoom[roomPos.x][roomPos.y].draw()
     boi.draw();
     overlay.draw()
 }
--- items.js
+++ items.js
@@ -1,73 +1,53 @@
-let Swordchance=100
-let itemsInRoom = [[]]
-let itemInv = []
+let Chance_Sword = 100;
+let Items_Rooms = [[]]
 
 function Sword(){
-    this.pos = createVector(width/2,300)
-    this.size = createVector(20,50)
-    this.color = "black"
-    
+    this.pos = createVector(width/1.5,150)
+    this.size = createVector(0,0)
+    this.InvPlace = 10
+
+    this.draw = function(){
+        fill("black")
+        rect(this.pos.x,this.pos.y,this.size.x,this.size.y)
+    }
+
     this.active = function(){
-        console.log("Pew")
+
+    }
+}
+
+
+function ItemsInRoom(){
+    this.itemArr = []
+
+    this.spawn = function(){
+        this.random = random(0,100)
+
+        if(this.random<=Chance_Sword){
+            this.itemArr.push(new Sword);
+        }
+    }
+
+    this.collect = function(i){
+        if(mag(Items_Rooms[roomPos.x][roomPos.y].itemArr[i].pos.x-boi.pos.x , Items_Rooms[roomPos.x][roomPos.y].itemArr[i].pos.y-boi.pos.y)<10)
+        console.log("YETTTTttt")
     }
 
     this.draw = function(){
-        fill(this.color)
-        rect(this.pos.x,this.pos.y,this.size.x,this.size.y)
+        for(let i = itemArr.length-1;i>-1;i--){
+            this.itemArr[i].draw();
+            this.collect(i);
+        }
     }
 }
 
 function SpawnItems(){
-    
-    this.spawnItems = function(){
-        if( itemsInRoom[roomPos.x][roomPos.y] == undefined){    
-            this.random= random(0,100)
-            itemsInRoom[roomPos.x][roomPos.y] = new Items(); 
-            if(Swordchance>=this.random){
-                if(roomInArr(itemsInRoom[roomPos.x][roomPos.y].items)<100){
-                    itemsInRoom[roomPos.x][roomPos.y].items[roomInArr(itemsInRoom[roomPos.x][roomPos.y].items)] = new Sword();
-                }
-            }
 
-            // More IFs for more rdm weapons
-        }
+    if(Items_Rooms[roomPos.x] == undefined) {
+        rooms[roomPos.x] = []
     }
-}
-
-function Items(){
-    this.itemcnt=0
-    this.items = []
-    this.length = 0
-   
-    this.draw=function(){
-        this.length = this.items.length;
-        this.collect();      
-        for(let i=0;i<this.length;i++){
-            if(this.items[i]!=undefined){
-                this.items[i].draw();
-            }
-        }
-    }
-
-    this.collect =function(){
-        for(let i=0;i<this.length;i++){
-            if(this.items[i]!=undefined){
-                if(mag(boi.pos.x-this.items[i].pos.x,boi.pos.y-this.items[i].pos.y)){
-                    if(roomInArr(itemInv)<10){
-                        itemInv[roomInArr(itemInv)]=this.items[i];
-                        this.items[i]=undefined;
-                    }
-                }
-            }
-        }
-    }
-}
-
-function roomInArr(arr){
-    for(let i=0;i<=30;i++){
-        if(arr[i]==undefined){
-            return i;
-        }
-        return 1000000000000;
+    if(Items_Rooms[roomPos.x][roomPos.y]==undefined){
+        Items_Rooms[roomPos.x][roomPos.y]= new ItemsInRoom();
+        Items_Rooms[roomPos.x][roomPos.y].spawn();
     }
 }
