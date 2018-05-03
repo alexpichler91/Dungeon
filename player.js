44b1c0dc6e5bb984277b7c1e3d8e79917b79e1f9
Alexander Pichler <alexander.pichler91@icloud.com>
2018-05-03 18:00:45 +0200

    collisions

--- player.js
+++ player.js
@@ -2,27 +2,40 @@ function Player() {
     let obs                              // private obstacle
     this.pos = createVector(0, 0)        // Position des Spielers
     this.size = 20;                      // Größe des Spielers
-    this.vel = createVector(0, 0)        // Beschleunigung ...
-    this.acc = 0.9;                      // Wie schnell er Beschleunigt
+    this.vel = createVector(0, 0)        // Geschwindigkeit ...
+    this.maxSpeed = 3                    // maximale Geschwindigkeit
+    this.acc = 0.9;                      // Wie schnell er (pro frame) Beschleunigt
 
     this.draw = function(){
         if(!dead){
-            if(keyIsDown(87)){
+            if(keyIsDown(87)) {
                 this.vel.y-=this.acc;
             }
-            if(keyIsDown(83)){
+            if(keyIsDown(83)) {
                 this.vel.y+=this.acc;
             }
-            if(keyIsDown(65)){
+            if(keyIsDown(65)) {
                 this.vel.x-=this.acc;
             }
-            if(keyIsDown(68)){
+            if(keyIsDown(68)) {
                 this.vel.x+=this.acc;
             }
 
             this.roomCollider()
             this.obstacleCollider()
             this.doorCollider()
+            if(this.vel.x >= this.maxSpeed) {
+                this.vel.x = this.maxSpeed
+            }
+            if(this.vel.x <= -this.maxSpeed) {
+                this.vel.x = -this.maxSpeed
+            }
+            if(this.vel.y >= this.maxSpeed) {
+                this.vel.y = this.maxSpeed
+            }
+            if(this.vel.y <= -this.maxSpeed) {
+                this.vel.y = -this.maxSpeed
+            }
             this.vel.mult(0.88)
             this.pos.add(this.vel)
         }
@@ -36,20 +49,20 @@ function Player() {
     }
 
     this.roomCollider = function() {
-        if(collideRectCircle(createVector(rooms[roomPos.x][roomPos.y].size.x / 2, 0), createVector(0, rooms[roomPos.x][roomPos.y].size.y), this.pos, this.size / 2 + 5) && boi.vel.x > 0){
+        if(collideRectCircle(createVector(rooms[roomPos.x][roomPos.y].size.x / 2, 0), createVector(0, rooms[roomPos.x][roomPos.y].size.y), this.pos, this.size / 2) && this.vel.x > 0){
             this.pos.x = (rooms[roomPos.x][roomPos.y].size.x - this.size) / 2
             this.vel.x = 0
         }
-        if(collideRectCircle(createVector(-rooms[roomPos.x][roomPos.y].size.x / 2, 0), createVector(0, rooms[roomPos.x][roomPos.y].size.y), this.pos, this.size / 2 + 5) && boi.vel.x < 0){
+        if(collideRectCircle(createVector(-rooms[roomPos.x][roomPos.y].size.x / 2, 0), createVector(0, rooms[roomPos.x][roomPos.y].size.y), this.pos, this.size / 2) && this.vel.x < 0){
             this.pos.x = -(rooms[roomPos.x][roomPos.y].size.x - this.size) / 2
             this.vel.x = 0
         }
 
-        if(collideRectCircle(createVector(0, rooms[roomPos.x][roomPos.y].size.y / 2), createVector(rooms[roomPos.x][roomPos.y].size.x, 0), this.pos, this.size / 2 + 5) && boi.vel.y > 0){
+        if(collideRectCircle(createVector(0, rooms[roomPos.x][roomPos.y].size.y / 2), createVector(rooms[roomPos.x][roomPos.y].size.x, 0), this.pos, this.size / 2) && this.vel.y > 0){
             this.pos.y = (rooms[roomPos.x][roomPos.y].size.y - this.size) / 2
             this.vel.y = 0
         }
-        if(collideRectCircle(createVector(0, -rooms[roomPos.x][roomPos.y].size.y / 2), createVector(rooms[roomPos.x][roomPos.y].size.x, 0), this.pos, this.size / 2 + 5) && boi.vel.y < 0){
+        if(collideRectCircle(createVector(0, -rooms[roomPos.x][roomPos.y].size.y / 2), createVector(rooms[roomPos.x][roomPos.y].size.x, 0), this.pos, this.size / 2) && this.vel.y < 0){
             this.pos.y = -(rooms[roomPos.x][roomPos.y].size.y - this.size) / 2
             this.vel.y = 0
         }
@@ -59,6 +72,15 @@ function Player() {
         for(obs of rooms[roomPos.x][roomPos.y].obstacles) {
             if(this.vel.x > 0 && collideRectCircle(createVector(obs.pos.x - (obs.size.x / 2), obs.pos.y), createVector(0, obs.size.y), this.pos, this.size / 2)) {
                 this.vel.x = 0
+            }
+            if(this.vel.x < 0 && collideRectCircle(createVector(obs.pos.x + (obs.size.x / 2), obs.pos.y), createVector(0, obs.size.y), this.pos, this.size / 2)) {
+                this.vel.x = 0
+            }
+            if(this.vel.y > 0 && collideRectCircle(createVector(obs.pos.y - (obs.size.y / 2), obs.pos.x), createVector(obs.size.x, 0), this.pos, this.size / 2)) {
+                this.vel.y = 0
+            }
+            if(this.vel.y < 0 && collideRectCircle(createVector(obs.pos.y + (obs.size.y / 2), obs.pos.x), createVector(obs.size.x, 0), this.pos, this.size / 2)) {
+                this.vel.y = 0
             }
         }
     }
